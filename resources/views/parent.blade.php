8924e9c4fa6d5d9008d5f0058164f5cb09c4f404
FirtaArieFirmansyah <firta10rpl1@gmail.com>
2022-10-25 13:29:33 +0700

    Merubah parent home ke /

--- resources/views/parent.blade.php
+++ resources/views/parent.blade.php
@@ -26,7 +26,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link @if(Request::is('home')) active @endif" aria-current="page" href="/home">Home</a>
+              <a class="nav-link @if(Request::is('home')) active @endif" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link @if(Request::is('about')) active @endif" href="/about">About </a>
